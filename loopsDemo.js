dcc3277e7f1ce4f651d1d2586b5c5090ef3f5a9b
lohitha-anumala <68274764+lohitha-anumala@users.noreply.github.com>
2020-07-23 06:16:36 +0530

    Add files via upload

---
+++ loopsDemo.js
@@ -0,0 +1,20 @@
+console.log('example  of do-while')
+var text = "";
+var i = 0;
+do {
+    console.log(i)
+  text += "The number is " + i;
+  i++;
+  
+}
+while (i < 5);
+
+console.log('example of while')
+// while example
+var text = "";
+var i = 0;
+while (i < 5) {
+   console.log(i)
+  text += "<br>The number is " + i;
+  i++;
+}
---
+++ switchjs.js
@@ -0,0 +1,29 @@
+//var x=1;
+//while(x < 10){
+  //  console.log(x);
+   // x++;
+//}
+
+
+let x="fruits"
+ switch(x) {
+    case x: 
+    
+        console.log(`hello`);
+        break;
+        case y:
+       console.log(`welcome`);
+        break;
+        default:
+            console.log(`better luck next nxt Time`);
+      }
+
+
+
+//let i=0;
+  //      do{
+    //        console.log(`The number is `+ i);
+      //      i++;
+
+        //}
+        //while(i<5);
